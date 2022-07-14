8b0fa1e6417fcdb1edb835d7540e445c7c11ff08
DubTech-Web <103583108+DubTech-Web@users.noreply.github.com>
2022-07-14 10:47:16 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+var character=document.getElementById("character");
+var block =document.getElementById("block");
+var block2 =document.getElementById("block2");
+function jump() {
+  if(character.classList != "animate" ){
+      character.classList.add("animate")
+  }
+  setTimeout(function () {
+    character.classList.remove("animate");
+    },500);
+}
+var checkDead =setInterval(function (){
+ 
+ var characterTop =
+  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockLeft =
+  parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+     block.style.animation="none";
+     block.style.display="none";
+     var blockLeft =
+  
+    alert("You Lose")
+    alert("Please refresh the browser to start again")
+  };
+  
+},10)
